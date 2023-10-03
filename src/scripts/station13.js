385653b5be5245e4329df2bedcd3af700d5dfdf0
rassu <senyopan@hotmail.com>
2023-10-03 16:27:39 +0900

    課題クリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function changeBackgroundColor(e) {
-  // ここに背景色を変える処理を書く。
-
+  const text = document.getElementById("text");
+  if (check.checked){
+    text.style.backgroundColor = "red";
+  } else if(!check.checked){
+    text.style.backgroundColor = "transparent";
+  }
 }
